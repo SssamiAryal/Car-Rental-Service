1d32e49cd781ff5057a7cb9f9d9c9a8bac8bd510
SssamirAryal <166592937+SssamiAryal@users.noreply.github.com>
2025-07-24 18:24:04 +0545

    UPload and middlware updated

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/uploads", express.static("uploads"));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/booking", bookingRoutes);
 app.use("/api/vehicle", vehicleRoutes);
