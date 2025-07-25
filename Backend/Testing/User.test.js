c2d8be94eb24a55f88db534b9536813b4fa05149
SssamirAryal <166592937+SssamiAryal@users.noreply.github.com>
2025-07-25 18:23:26 +0545

    User testing updated

--- Backend/Testing/User.test.js
+++ Backend/Testing/User.test.js
@@ -19,7 +19,4 @@ describe("User Model", () => {
     expect(user.email).toBe("alice@example.com");
     expect(user.password).toBe("securepassword");
   });
-
-  // Note: sequelize-mock doesn't enforce validations like unique or allowNull,
-  // so to test those, you'd need integration or custom validation tests.
 });
